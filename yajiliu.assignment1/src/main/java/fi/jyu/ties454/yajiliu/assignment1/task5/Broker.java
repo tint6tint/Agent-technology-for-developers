985b1db70f13698fedcfe01a19773e4f63563271
Yanjun Liu <yanjun@YanJuns-MacBook-Pro.local>
2016-02-04 20:44:42 +0200

    Error fixed

--- yajiliu.assignment1/src/main/java/fi/jyu/ties454/yajiliu/assignment1/task5/Broker.java
+++ yajiliu.assignment1/src/main/java/fi/jyu/ties454/yajiliu/assignment1/task5/Broker.java
@@ -2,8 +2,8 @@ package fi.jyu.ties454.yajiliu.assignment1.task5;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.Random;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,62 +11,67 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
-import jade.core.behaviours.WakerBehaviour;
+
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class Broker extends Agent {
 
 	@Override
 	protected void setup() {
-		Map<String, Integer> map = new HashMap<String, Integer>();
+		Map<String, Integer> mapPublisher = new HashMap<String, Integer>();
+
+		Map<String, AID> mapSuberscribers = new HashMap<String, AID>();
 
 		addBehaviour(new CyclicBehaviour() {
 			@Override
 			public void action() {
 				// TODO Auto-generated method stub
-				ACLMessage recMessage = blockingReceive();
-				if (recMessage.getPerformative() == ACLMessage.INFORM) {
-					String receiveJsonMessage = recMessage.getContent();
+				MessageTemplate tInForm = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+				MessageTemplate tSUBSCRIBE = MessageTemplate.MatchPerformative(ACLMessage.SUBSCRIBE);
+				ACLMessage recMessageFromPublisher = receive(tInForm);
+				ACLMessage recMessageFromSubscriber = receive(tSUBSCRIBE);
+				/*
+				 * updated publisher list
+				 */
+				if (recMessageFromPublisher != null) {
+					String receiveJsonMessage = recMessageFromPublisher.getContent();
 					Message temp = new Message();
 					ObjectMapper mapper = new ObjectMapper();
 					try {
 						temp = mapper.readValue(receiveJsonMessage, Message.class);
 						String topic = temp.getTopic();
 						int content = temp.getContent();
-						map.put(topic, content);
+						mapPublisher.put(topic, content);
 
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 
-				} else if (recMessage.getPerformative() == ACLMessage.CFP) {
-					
-							String topicPicker = recMessage.getContent();
-							int updatedContent = map.get(topicPicker);
-							Message temp = new Message();
-							temp.setTopic(topicPicker);
-							temp.setContent(updatedContent);
+				}
+				/*
+				 * updated subscriber list
+				 */
+				if (recMessageFromSubscriber != null) {
+					String topicPicker = recMessageFromSubscriber.getContent();
+					AID subscribersID = recMessageFromSubscriber.getSender();
+					mapSuberscribers.put(topicPicker, subscribersID);
+				}
+				/*
+				 * in order to keep sending the updated message so it iterated all the subscribers
+				 */
+				if (recMessageFromSubscriber != null && recMessageFromPublisher != null) {
+					for (Map.Entry<String, AID> entry : mapSuberscribers.entrySet()) {						
+						if(mapPublisher.containsKey(entry.getKey())){
 							ACLMessage replyContentToSubscriber = new ACLMessage(ACLMessage.INFORM);
-							replyContentToSubscriber.addReceiver(new AID("Subscribers", AID.ISLOCALNAME));
-
-							ObjectMapper mapper = new ObjectMapper();
-							String jsonInString;
-							try {
-								jsonInString = mapper.writeValueAsString(temp);
-								replyContentToSubscriber.setContent(jsonInString);
-							} catch (JsonProcessingException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-
+							replyContentToSubscriber.addReceiver(entry.getValue());
+							int content= mapPublisher.get(entry.getKey());
+							replyContentToSubscriber.setContent(Integer.toString(content));
 							send(replyContentToSubscriber);
-						
-					
-					
-
+						}
+					}
 				} else {
-					System.out.println("didnt work");
 					block();
 				}
 			}
